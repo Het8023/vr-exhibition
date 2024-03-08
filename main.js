8afc6cd517db60480cc050652f9a4ebdcc99c5a6
Het8023 <858776802@qq.com>
2024-03-08 14:59:36 +0800

    前四个场景

---
+++ main.js
@@ -0,0 +1,376 @@
+import "./style.css";
+
+// 引入初始化场景
+import "./utils/init";
+
+// 引入threejs
+import * as THREE from "three";
+
+// 引入gui工具
+import guiMove from "./utils/gui.js";
+
+// 引入将dom 2d 转化为3d方法
+import { CSS3DObject } from "three/addons/renderers/CSS3DRenderer.js";
+
+// 创建分组
+let group = new THREE.Group();
+
+// 引入初始化场景
+import { scene, camera } from "./utils/init.js";
+
+// 准备纹理贴图所需要的数据
+const sceneInfoObj = {
+  //第一个场景的数据
+  one: {
+    // 纹理加载器公共路径
+    publicPath: "technology/1/",
+    // 纹理加载器需要加载的图片
+    imgUrlArr: ["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"],
+    // 准备标记点的数据
+    markList: [
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [-0.46, -0.11, -0.11],
+        // 物体的旋转角度
+        rotation: [1.42, 0.68, 1.63],
+        // 切换的下一个场景
+        targetAttr: "two",
+      },
+    ],
+  },
+  //第二个场景的数据
+  two: {
+    // 纹理加载的公共资源路径
+    publicPath: "technology/2/",
+    // 纹理加载需要加载的图片资源
+    imgUrlArr: ["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"],
+    // 准备标记点的数据, 当前空间中所有标记信息对象
+    markList: [
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [0.35, -0.09, 0.03],
+        // 物体的旋转角度
+        rotation: [4.72, 0.89, 2.36],
+        // 切换的下一个场景
+        targetAttr: "one",
+      },
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [-0.46, -0.11, -0.11],
+        // 物体的旋转角度
+        rotation: [1.42, 0.68, 1.63],
+        // 切换的下一个场景
+        targetAttr: "three",
+      },
+    ],
+  },
+  // 第三个场景的数据
+  three: {
+    // 纹理加载的公共资源路径
+    publicPath: "technology/3/",
+    // 纹理加载需要加载的图片资源
+    imgUrlArr: ["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"],
+    // 准备标记点的数据, 当前空间中所有标记信息对象
+    markList: [
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [0.4, -0.18, 0.32],
+        // 物体的旋转角度
+        rotation: [-1.53, -0.04, -1.26],
+        // 切换的下一个场景
+        targetAttr: "two",
+      },
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [0.32, -0.16, -0.33],
+        // 物体的旋转角度
+        rotation: [1.46, 0.1, -0.17],
+        // 切换的下一个场景
+        targetAttr: "four",
+      },
+    ],
+  },
+  // 第四个场景的数据
+  four: {
+    // 纹理加载的公共资源路径
+    publicPath: "technology/4/",
+    // 纹理加载需要加载的图片资源
+    imgUrlArr: ["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"],
+    markList: [
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [-0.35, -0.22, 0.4],
+        // 物体的旋转角度
+        rotation: [-0.85, -0.45, -1.8],
+        // 切换的下一个场景
+        targetAttr: "three",
+      },
+      {
+        // 标记点名称
+        name: "dom",
+        // 物体的位置坐标
+        position: [0.49, 0, 0],
+        // 物体的旋转角度
+        rotation: [0, -0.5 * Math.PI, 0],
+        // 切换的下一个场景
+        targetAttr: "five",
+        // 回调函数
+        active() {
+          setMaterialCube(sceneInfoObj.five);
+        },
+      },
+    ],
+  },
+  // 第五个场景的数据
+  five: {
+    // 纹理加载的公共资源路径
+    publicPath: "technology/5/",
+    // 纹理加载需要加载的图片资源
+    imgUrlArr: ["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"],
+    markList: [
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [-0.05, -0.08, 0.46],
+        // 物体的旋转角度
+        rotation: [5.41, 2.91, 4.79],
+        // 切换的下一个场景
+        targetAttr: "four",
+      },
+      {
+        // 标记点名称
+        name: "landMark",
+        // 标记点图片的路径
+        imgUrl: "other/landmark.png",
+        // 物体的宽度
+        wh: [0.05, 0.05],
+        // 物体的位置坐标
+        position: [0.05, 0.08, 0.46],
+        // 物体的旋转角度
+        rotation: [5.41, 2.91, 4.79],
+        // 切换的下一个场景
+        targetAttr: "six",
+      },
+      {
+        // 标记点名称
+        name: "video",
+        // 视频路径
+        imgUrl: "video/movie.mp4",
+        // 物体的宽高
+        wh: [0.2, 0.1],
+        // 物体的位置坐标
+        position: [0.49, 0.04, 0.045],
+        // 物体的旋转角度
+        rotation: [0, -0.5 * Math.PI, 0],
+      },
+    ],
+  },
+};
+
+// 创建立方缓冲几何体
+function createCube() {
+  // 创建图形
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // 创建材质
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    side: THREE.DoubleSide,
+  });
+
+  // 创建物体对象, 并且将图形与材质渲染到物体
+  const cube = new THREE.Mesh(geometry, material);
+
+  // 调整立方体沿着 z 轴做 -1 缩小（镜面翻转）
+  cube.scale.set(1, 1, -1);
+
+  // 将立方缓冲几何体添加到组
+  // group.add(cube);
+
+  // 将分组添加到场景
+  scene.add(cube);
+
+  // 将物体对象返回给函数
+  return cube;
+}
+
+// 创建纹理加载器，并对立方缓冲几何体进行贴图
+function setMaterialCube(infoObj) {
+  // 清除上一个场景的标记点
+  clear();
+
+  const { publicPath, imgUrlArr, markList } = infoObj;
+  // 创建纹理加载器
+  const texturlLoader = new THREE.TextureLoader();
+  // 设置纹理加载器的公共资源路径
+  texturlLoader.setPath(publicPath);
+  // 通过纹理加载器加载图片资源，并创建对应材质
+  const materialArr = imgUrlArr.map((item) => {
+    const texturl = texturlLoader.load(item);
+
+    // 设置图片的颜色通道
+    texturl.colorSpace = THREE.SRGBColorSpace;
+    // 创建材质
+    return new THREE.MeshBasicMaterial({
+      map: texturl,
+      side: THREE.DoubleSide,
+    });
+  });
+
+  // 将立方缓冲几何体的材质设置为贴图之后的材质
+  cubeObj.material = materialArr;
+
+  // 循环遍历标记点
+  markList.forEach((item) => {
+    // 如果场景里面存在标记点，则调用创建标记点方法
+    if (item.name === "landMark") createLandMark(item);
+    else if (item.name === "dom") createDomMark(item);
+  });
+  scene.add(group);
+}
+
+// 创建标记点贴图
+function createLandMark(infoObj) {
+  const { name, imgUrl, wh, position, rotation, targetAttr } = infoObj;
+  // 创建纹理加载器
+  const texturlLoader = new THREE.TextureLoader();
+
+  // 创建图形（平面缓冲几何体）
+  const geometry = new THREE.PlaneGeometry(...wh);
+  // 常见材质
+  const material = new THREE.MeshBasicMaterial({
+    // 贴图
+    map: texturlLoader.load(imgUrl),
+    // 双面渲染
+    side: THREE.DoubleSide,
+    // 设置透明
+    transparent: true,
+  });
+  // 创建物体对象，并且把图形与材质渲染到物体对象
+  const plane = new THREE.Mesh(geometry, material);
+
+  // 设置物体的坐标
+  plane.position.set(...position);
+
+  // 设置物体的旋转角度
+  plane.rotation.set(...rotation);
+
+  // 设置标记点名称
+  plane.name = name;
+  // 将标记点的数据添加到物体上
+  plane.userData.targetAttr = targetAttr;
+
+  // 调用gui工具
+  guiMove(plane);
+
+  group.add(plane);
+
+  // 将物体对象添加到场景
+  // scene.add(plane);
+}
+
+// 创建文本标记点
+function createDomMark(infoObj) {
+  let { position, rotation, active } = infoObj;
+  const tag = document.createElement("span");
+  tag.className = "mark-style";
+  tag.innerHTML = "前进";
+  tag.style.color = "#fff";
+  tag.style.pointerEvents = "all";
+  tag.addEventListener("click", (e) => {
+    active(e);
+  });
+
+  const tag3d = new CSS3DObject(tag);
+  tag3d.scale.set(1 / 800, 1 / 800, 1 / 800);
+  tag3d.position.set(...position);
+  tag3d.rotation.set(...rotation);
+  group.add(tag3d);
+}
+
+// 清除上一个场景的标记点
+function clear() {
+  // console.log(group);
+  const list = [...group.children];
+  list.forEach((obj) => {
+    if (!obj.isCSS3DObject) {
+      obj.geometry.dispose();
+      obj.material.dispose();
+    }
+    group.remove(obj);
+  });
+}
+
+// 给3d场景添加点击事件
+function bindClick() {
+  const raycaster = new THREE.Raycaster();
+  const pointer = new THREE.Vector2();
+
+  window.addEventListener("click", (event) => {
+    // 将鼠标位置归一化为坐标位置，x和y方向的取值范围是（-1 to =1）
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // 通过摄像机和鼠标位置更新射线
+    raycaster.setFromCamera(pointer, camera);
+
+    // 计算物体和射线的焦点
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    const obj = intersects.find((item) => item.object.name == "landMark");
+
+    if (obj) {
+      // 当点击了标记点，切换场景的数据
+      const infoObj = sceneInfoObj[obj.object.userData.targetAttr];
+
+      if (infoObj) setMaterialCube(infoObj);
+    }
+  });
+}
+
+// 调用创建立方缓冲几何体方法
+const cubeObj = createCube();
+
+bindClick();
+// 对立方缓冲几何体进行贴图
+setMaterialCube(sceneInfoObj.one);
---
+++ utils/init.js
@@ -0,0 +1,106 @@
+// 主要是用来初始化THREEJS的基础环境
+
+// 初始化场景，摄像机，渲染器，轨道控制器，坐标轴，场景适配，循环渲染
+
+// 引入threeJS
+import * as THREE from "three";
+
+// 引入轨道控制器
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// 引入将原生DOM转化为3D并且渲染到3D场景的方法(引入3d转换器与渲染器)
+import { CSS3DRenderer } from "three/addons/renderers/CSS3DRenderer.js";
+
+// 声明场景、摄像机、3D渲染器、轨道控制器、3D原生DOM渲染器等全局变量
+export let scene, camera, renderer, controls, css3dRenderer;
+
+// 初始化场景
+(function init() {
+  // 创建场景
+  scene = new THREE.Scene();
+
+  // 创建摄像机
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  //   初始化相机位置
+  camera.position.z = 0.1;
+
+  // 创建渲染器（画布）
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  //   设置画布大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // 将渲染器添加到dom
+  document.body.appendChild(renderer.domElement);
+})();
+
+// 创建轨道控制器
+(function cretaeControls() {
+  // 创建轨道控制器
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // 垂直方向旋转角度
+  //   controls.maxPolarAngle = Math.PI * 0.25;
+
+  //   关闭阻尼效果
+  controls.enableDamping = false;
+})();
+
+// 将原生dom3d渲染到3D场景中
+(function create3dRenderer() {
+  // 创建文本3d渲染器
+  css3dRenderer = new CSS3DRenderer();
+  // 设置文本渲染器的大小
+  css3dRenderer.setSize(window.innerWidth, window.innerHeight);
+  // 默认去除dom事件
+  css3dRenderer.domElement.style.pointerEvents = "none";
+  // 设置固定定位
+  css3dRenderer.domElement.style.position = "fixed";
+  // 设置距离左侧间距为0
+  css3dRenderer.domElement.style.left = 0;
+  // 设置距离上侧间距为0
+  css3dRenderer.domElement.style.top = 0;
+  // 添加到body中
+  document.body.appendChild(css3dRenderer.domElement);
+})();
+
+// 坐标轴
+(function createHelper() {
+  // 创建坐标轴
+  const axesHelper = new THREE.AxesHelper(8);
+  // 将坐标轴添加到场景中
+  scene.add(axesHelper);
+})();
+
+// 场景适配
+(function resizeRender() {
+  window.addEventListener("resize", () => {
+    // 自适应摄像机的宽度比
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // 自适应画布的宽高比
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // 重新更新垂体空间
+    camera.updateProjectionMatrix();
+  });
+})();
+
+// 循环渲染
+(function renderLoop() {
+  // 渲染更新渲染
+  requestAnimationFrame(renderLoop);
+
+  // 通过轨道控制器调用场景更新
+  controls.update();
+
+  // 将场景和摄像机渲染到画布上面
+  renderer.render(scene, camera);
+
+  //   将场景和摄像机渲染到文本画布上面
+  css3dRenderer.render(scene, camera);
+})();
